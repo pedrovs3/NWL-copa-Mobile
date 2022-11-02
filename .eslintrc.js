bb931e965e6c7232125d17dc22db032c871cd346
Pedro Vieira <pedrovs3@hotmail.com>
2022-11-02 11:58:43 -0300

    Criando loading e configurando a splash screen

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:react/recommended',
+  ],
+  parser: '',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: '@typescript-eslint/parser',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    'import/prefer-default-export': 'off',
+    camelcase: 'off',
+    'import/extensions': 'off',
+  },
+};
---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/styles/theme';
+import Loading from './src/components/Loading';
+import { Signin } from './src/screens/Signin';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
+  return (
+        <NativeBaseProvider theme={THEME}>
+          <StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent/>
+              { fontsLoaded ? <Signin/> : <Loading/> }
+        </NativeBaseProvider>
+  );
+}
